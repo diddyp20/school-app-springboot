86c453920dbe625c42f491de586416cc8f6eb4d3
Didier <diddyp20@users.noreply.github.com>
2021-07-17 00:46:55 -0400

    added caching for individual classes

--- src/main/java/com/bndnetworks/net/schoolAppdemo/config/LocalCacheConfig.java
+++ src/main/java/com/bndnetworks/net/schoolAppdemo/config/LocalCacheConfig.java
@@ -1,5 +1,6 @@
 package com.bndnetworks.net.schoolAppdemo.config;
 
+import com.bndnetworks.net.schoolAppdemo.classes.entity.ClassEntity;
 import com.bndnetworks.net.schoolAppdemo.classes.repository.ClassRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -9,6 +10,8 @@ import org.springframework.cache.CacheManager;
 import org.springframework.context.ApplicationListener;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 import static com.bndnetworks.net.schoolAppdemo.util.SchoolCacheConstant.*;
 
@@ -22,6 +25,7 @@ public class LocalCacheConfig implements ApplicationListener<ApplicationStartedE
 
     @Autowired
     ClassRepository classRepository;
+
     @Override
     public void onApplicationEvent(ApplicationStartedEvent applicationStartedEvent) {
         LOGGER.info("appName: School App Demo || message: Initializing local caches.");
@@ -30,12 +34,26 @@ public class LocalCacheConfig implements ApplicationListener<ApplicationStartedE
 
     }
 
-     void populateCaches() {
-        try{
-            classRepository.findAllBy();
+    void populateCaches() {
+        try {
+            List<ClassEntity> classList = classRepository.findAllBy();
+            if (!classList.isEmpty()) {
+                populateIndividualClass(classList);
+            }
             classRepository.findById(1);
-        }catch (Exception e){
+        } catch (Exception e) {
             LOGGER.error("appName: School App Demo || Critical Error retrieving local cache");
+        }
+    }
+
+    private void populateIndividualClass(List<ClassEntity> classList) {
+        for (ClassEntity individualClass : classList) {
+            Integer classId = individualClass.getId();
+            try {
+                classRepository.findById(classId);
+            } catch (Exception e) {
+                LOGGER.error("appName: School App Demo || Critical Error retrieving individual classes");
+            }
         }
     }
 }
